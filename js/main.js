a704c4b8d7a3a9ea985deb875895acaf5241d97f
Kannan <38552763+kannangs84@users.noreply.github.com>
2021-02-23 21:33:48 +0530

    Add files via upload
    Updated js file

--- js/main.js
+++ js/main.js
@@ -132,6 +132,19 @@ jQuery(document).ready(function($) {
     }
   });
 
+  // Porfolio isotope and filter
+  $(window).on('load', function () {
+    var portfolioIsotope = $('.portfolio-container').isotope({
+      itemSelector: '.portfolio-item'
+    });
+    $('#portfolio-flters li').on( 'click', function() {
+      $("#portfolio-flters li").removeClass('filter-active');
+      $(this).addClass('filter-active');
+  
+      portfolioIsotope.isotope({ filter: $(this).data('filter') });
+    });
+  });
+
   // custom code
   // Testimonials carousel (uses the Owl Carousel library)
   $(".testimonials-carousel").owlCarousel({
